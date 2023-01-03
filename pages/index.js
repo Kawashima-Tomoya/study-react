7ea13bb08a60aef01e4b3c884af75b54bd9e6066
Kawashima-Tomoya <57kawashima@gmail.com>
2023-01-03 17:10:13 +0900

    ♻️ export default の削除

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Links from '../components/Links'
-import Icons from '../components/Icons'
-import Headline from '../components/Headline'
+import { Links } from '../components/Links'
+import { Icons } from '../components/Icons'
+import { Headline } from '../components/Headline'
 
 export default function Home() {
   return (
