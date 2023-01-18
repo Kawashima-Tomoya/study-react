fab5e4b728763a28716939a89df738cbebc527de
Kawashima-Tomoya <57kawashima@gmail.com>
2023-01-18 11:11:11 +0900

    ⚗️ クリックイベントの検証（そのうち消す）

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,28 @@
 import Head from 'next/head'
+import { useCallback } from 'react';
 import { Header } from 'src/components/Header'
 import { Main } from 'src/components/Main'
 
 export default function Home() {
+
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  },[]);
+  
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
+      <a href="/about"
+         onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Header />
       <Main page="index" />
     </>
