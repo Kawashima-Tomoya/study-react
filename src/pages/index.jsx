ec5df9358c8953ae12f176e927652944278af5bb
Kawashima-Tomoya <57kawashima@gmail.com>
2023-01-25 20:30:07 +0900

    ⚡️ useCallbackとuseEffectの第二引数に関して説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,35 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import styles from "src/styles/Home.module.css"
+import { useCallback, useEffect, useState } from 'react'
 import { Header } from 'src/components/Header'
 import { Main } from 'src/components/Main'
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  // const [text,, setText] = useState("");
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1 );
-    setCount((count) => count + 1 );
-    // setFoo(function(count) {
-    //   return (count + 1);
-    // });
-  };
+  const handleClick = useCallback(() => {
+    if(count < 10) {
+      setCount((count) => count + 1 );
+      // setFoo(function(count) {
+      //   return (count + 1);
+      // });
+    }
+  },[count]);
+
+  // const handleChange = (e) => {
+  //   if (text.length >= 5) {
+  //     return;
+  //   }
+  //   setText(e.target.value);
+  // };
 
   useEffect (()=> {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
-  },[]);
+  },[count]);
 
   return (
     <>
@@ -29,6 +39,8 @@ export default function Home() {
       <Header />
       <h1>{count}</h1>
       <button onClick={handleClick}>ボタン</button>
+      {/* <input type="text" value={text} 
+        onChange={handleChange} /> */}
       <Main page="index" />
     </>
   )
